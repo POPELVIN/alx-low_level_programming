00de87362858a80d042567fa4df5adcedb9a2ab9
POPELVIN <amitopopelvin47@gmail.com>
2023-02-18 13:07:32 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - prints the lower case of the alphabet
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	char lc;
+
+	lc = 'a';
+
+	while (lc <= 'z')
+	{
+		putchar(lc);
+		lc++;
+	}
+	putchar('\n');
+	return (0);
+}
