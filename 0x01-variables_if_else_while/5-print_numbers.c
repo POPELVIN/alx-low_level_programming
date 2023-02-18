606284078252675c99214ab479f6c5832568461e
POPELVIN <amitopopelvin47@gmail.com>
2023-02-18 13:20:57 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -is the entry  point
+ *
+ * Return: zero if no error
+ */
+int main(void)
+{
+	int n;
+	n = 0;
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
