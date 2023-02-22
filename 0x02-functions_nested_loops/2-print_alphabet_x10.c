e6aa474523eb554f3350867a420c05262d9c0403
POPELVIN <amitopopelvin47@gmail.com>
2023-02-22 11:20:39 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
-while (a <= 9)
-	{
-	for (m = 'a'; m <= 'z'; m++)
-	{
-		_putchar(m);
-	}
-		for (m = 'a'; m <= 'z'; m++)
-		{
-			_putchar(m);
-		}
-		_putchar('\n');
-		a++;
-	}
-_putchar('\n');
-a++;
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - prints all alphabets in lower case for 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	int count = 0;
+
+	while (count++ <= 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+	}
 }
