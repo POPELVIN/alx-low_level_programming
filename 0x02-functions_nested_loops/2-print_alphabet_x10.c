279f2b22ee05303c5b0a4fff32d9409034aa719f
POPELVIN <amitopopelvin47@gmail.com>
2023-02-22 10:42:59 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+while (a <= 9)
+	{
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		_putchar(m);
+	}
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+		a++;
+	}
+_putchar('\n');
+a++;
+
+}
