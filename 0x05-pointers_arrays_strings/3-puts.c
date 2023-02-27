ac2454e0719d95f20eff42c1376ee380cd0c9ebb
POPELVIN <amitopopelvin47@gmail.com>
2023-02-27 12:01:06 -0800

    Redo Task3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- */
+  * _puts - a function that prints a string, followed by a new line, to stdout.
+  * @str: string to be printed
+  * Return: void
+  */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
+		_putchar(*str);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
-
