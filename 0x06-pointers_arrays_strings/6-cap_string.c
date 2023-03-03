07a30aefd4d9864dbf7c8394b35bcf88257d846d
POPELVIN <amitopopelvin47@gmail.com>
2023-03-03 10:57:15 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,71 @@
 #include "main.h"
 
 /**
-  * cap_string - a function that capitalizes all words of a string.
-  * @str: single user input
-  * Return: Always 0.
-  */
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] = str[index] - 32;
+
+		index++;
+	}
+
+	return (str);
+}
+
+OR USING AN IF ELSE STATEMENT
 
 char *cap_string(char *str)
 {
-	int m = 0;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-	while (str[m])
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[m] >= 'a' && str[m] <= 'z')
-				m++;
-	}
-	if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
-		str[i - 1] == '}' || i == 0)
-	{
-	str[i] -= 32;
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
 	}
 	return (str);
 }
