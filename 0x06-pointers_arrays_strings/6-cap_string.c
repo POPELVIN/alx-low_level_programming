d1ffb1a8b6af74a0dd6cadaec71704d43922dd3d
POPELVIN <amitopopelvin47@gmail.com>
2023-03-03 11:25:12 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,39 +8,6 @@
  */
 char *cap_string(char *str)
 {
-	int index = 0;
-
-	while (str[index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] = str[index] - 32;
-
-		index++;
-	}
-
-	return (str);
-}
-
-OR USING AN IF ELSE STATEMENT
-
-char *cap_string(char *str)
-{
 	int i, c;
 	int trigger;
 	char nots[] = ",;.!?(){}\n\t\" ";
