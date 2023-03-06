bcdb9c7cd91b7649443678c2d96d84b5be4a2064
POPELVIN <amitopopelvin47@gmail.com>
2023-03-06 13:44:48 -0800

    100-main.c

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -12,7 +12,7 @@ char *s0 = "Bob Dylan";
 char *s1 = "Robert Allen";
 
 printf("%s, %s\n", s0, s1);
-void set_string(char **s, char *to);
+set_string(&s1, s0);
 printf("%s, %s\n", s0, s1);
 return (0);
 }
